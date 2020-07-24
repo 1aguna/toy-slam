a22013f0e9aec48d3a5258f2bf7dbb88bb650e45
1aguna <mlusher28@gmail.com>
2020-07-24 10:45:30 -0700

    Init commit

---
+++ toy-slam/main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import csv
+import copy
+import time
+import random
+import argparse
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--dir", type=str, default="/data/2011_09_29/2011_09_29_drive_0071_sync/")
+parser.add_argument("--maxIters", type=str, default=20)
+parser.add_argument("--loop_threshold", type=float, default=0.11)  # 0.11 is usually safe (for avoiding false loop closure)
+parser.add_argument("--color", type=bool, default=True)
+args = parser.parse_args()
+
+lidar_dir = os.path.join(args.dir, "velodyne_points", "data")
+
+if args.color:      # color images
+    img_dir = os.path.join(args.dir, "image_02", "data")
+else:               # black and white images
+    img_dir = os.path.join(args.dir, "image_00", "data")
+
